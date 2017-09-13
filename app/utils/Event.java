fd092a550b1da033eb530531c180bcc4c23c5c2a
Igor Stojadinovic <mightytroll@gmail.com>
2017-09-13 13:24:08 -0700

    XOL-3691 Adding old events for backward compatibility

--- app/utils/Event.java
+++ app/utils/Event.java
@@ -15,8 +15,11 @@ public final class Event {
     public static final String USER_AGENT_REMOVE = "user.agent.remove";
     public static final String USER_AGENT_UPDATE = "user.agent.update";
     public static final String USER_UPDATE = "user.update";
+    public static final String INSTALLATION_CREATE = "installation.create";
+    public static final String INSTALLATION_UPDATE = "installation.update";
+    public static final String INSTALLATION_DELETE = "installation.delete";
+    // Old events are kept for backwards compatibility. They can be removed after Legolas update.
     public static final String PLUGIN_INSTALL = "plugin.install";
     public static final String PLUGIN_CONFIG_UPDATE = "plugin.config.update";
     public static final String PLUGIN_UNINSTALL = "plugin.uninstall";
-
 }
